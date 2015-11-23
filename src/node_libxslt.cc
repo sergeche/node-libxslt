35a7b9c602bfcbd9209b17a04f15596c314279f4
Sergey Chikuyonok <serge.che@gmail.com>
2015-11-23 17:39:34 +0300

    Fixed error reporting for Win platform

--- src/node_libxslt.cc
+++ src/node_libxslt.cc
@@ -241,6 +241,54 @@ NAN_METHOD(RegisterEXSLT) {
 }
 
 /////// 4game
+
+void set_string_field(Local<Object> obj, const char* name, const char* value) {
+  Nan::HandleScope scope;
+  if (!value) {
+    return;
+  }
+  Nan::Set(obj, Nan::New<String>(name).ToLocalChecked(), Nan::New<String>(value, strlen(value)).ToLocalChecked());
+}
+
+void set_numeric_field(Local<Object> obj, const char* name, const int value) {
+  Nan::HandleScope scope;
+  Nan::Set(obj, Nan::New<String>(name).ToLocalChecked(), Nan::New<Int32>(value));
+}
+
+Local<Value> BuildSyntaxError(xmlError* error) {
+  Nan::EscapableHandleScope scope;
+
+  Local<Value> err = Exception::Error(Nan::New<String>(error->message).ToLocalChecked());
+  Local<Object> out = Local<Object>::Cast(err);
+
+  set_numeric_field(out, "domain", error->domain);
+  set_numeric_field(out, "code", error->code);
+  set_string_field(out, "message", error->message);
+  set_numeric_field(out, "level", error->level);
+  set_numeric_field(out, "column", error->int2);
+  set_string_field(out, "file", error->file);
+  set_numeric_field(out, "line", error->line);
+  set_string_field(out, "str1", error->str1);
+  set_string_field(out, "str2", error->str2);
+  set_string_field(out, "str3", error->str3);
+
+  // only add if we have something interesting
+  if (error->int1) {
+    set_numeric_field(out, "int1", error->int1);
+  }
+  return scope.Escape(err);
+}
+
+void PushToArray(void* errs, xmlError* error) {
+  Nan::HandleScope scope;
+  Local<Array> errors = *reinterpret_cast<Local<Array>*>(errs);
+  // push method for array
+  Local<Function> push = Local<Function>::Cast(errors->Get(Nan::New<String>("push").ToLocalChecked()));
+
+  Local<Value> argv[1] = { BuildSyntaxError(error) };
+  push->Call(errors, 1, argv);
+}
+
 int getXmlParserOption2(Local<Object> props, const char *key, int value) {
   Local<String> key2 = Nan::New<String>(key).ToLocalChecked();
   Local<Boolean> val = props->Get(key2)->ToBoolean();
@@ -280,7 +328,9 @@ xmlParserOption getXmlParserOption(Local<Object> props) {
 
 NAN_METHOD(ReadXmlFile) {
   Nan::HandleScope scope;
-  libxmljs::XmlSyntaxErrorsSync errors; // RAII sentinel
+  Local<Array> errors = Nan::New<Array>();
+  xmlResetLastError();
+  xmlSetStructuredErrorFunc(reinterpret_cast<void*>(&errors), PushToArray);
   
   xmlParserOption opts = getXmlParserOption(info[1]->ToObject());
   String::Utf8Value filename(info[0]->ToString());
@@ -289,13 +339,13 @@ NAN_METHOD(ReadXmlFile) {
   if (!doc) {
     xmlError* error = xmlGetLastError();
     if (error) {
-      return Nan::ThrowError(libxmljs::XmlSyntaxErrorsSync::BuildSyntaxError(error));
+      return Nan::ThrowError(BuildSyntaxError(error));
     }
     return Nan::ThrowError("Could not parse XML file");
   }
 
   Local<Object> doc_handle = libxmljs::XmlDocument::New(doc);
-  Nan::Set(doc_handle, Nan::New<String>("errors").ToLocalChecked(), errors.ToArray());
+  Nan::Set(doc_handle, Nan::New<String>("errors").ToLocalChecked(), errors);
 
   xmlNode* root_node = xmlDocGetRootElement(doc);
   if (root_node == NULL) {
